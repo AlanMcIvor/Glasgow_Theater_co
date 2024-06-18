4f7b18eddca93aa9d119d18ce5c875d4b9120c9c
Your Name <you@example.com>
2024-06-18 19:56:59 +0100

    Glasgo Theater Co

---
+++ pages/shared/includes/header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta
+      name="title"
+      content="Glasgot Theater Company - A Premier Venue for Live Performances"
+    />
+    <meta
+      name="description"
+      content="Experience the magic of live theater at The Glasgow theater company. Discover upcoming shows, buy tickets, and enjoy an evening of unparalleled entertainment."
+    />
+    <meta
+      name="keywords"
+      content="theater, live performances, plays, musicals, concerts, ticket booking, upcoming shows, entertainment"
+    />
+    <meta name="author" content="Alan McIvor" />
+    <meta name="robots" content="index, follow" />
+
+    <link rel="stylesheet" href="../../css/reset.css" />
+    <link rel="stylesheet" href="../../css/main.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+      integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+
+    <title>Glasgow Theater Co</title>
+  </head>
+  <body>
+    <header class="header">
+      <nav class="navbar gutter flex">
+        <div class="logo">
+          <img
+            src="../../assets/icons/logo.png"
+            alt="The glasgow theater company logo"
+          />
+        </div>
+        <div>
+          <ul class="nav flex">
+            <li><a class="nav_link" href="#">Home</a></li>
+
+            <li><a class="nav_link" href="#">What's On</a></li>
+
+            <li><a class="nav_link" href="#">News</a></li>
+
+            <li><a class="nav_link" href="#">Events</a></li>
+
+            <li><a class="nav_link" href="#">Contact</a></li>
+          </ul>
+        </div>
+        <div class="btns">
+          <a href="#" class="main_btn btn flex"
+            ><span class="btn_span">Account</span>
+            <i class="fa-regular fa-user"></i>
+          </a>
+        </div>
+      </nav>
+    </header>
---
+++ pages/shared/index.php
@@ -0,0 +1,17 @@
+<?php
+
+include "./includes/header.php"
+
+?>
+
+<main class="hero">
+  <div class="hero_info">
+    <h3 class="hero_title">Grease: The Musical</h3>
+    <p class="hero_date">Tue 15 June - Sat 22 June</p>
+    <p class="hero_btns flex"><button class="btn"><span>Info</span></button><button class="btn main_btn">Tickets</button></p>
+  </div>
+</main>
+
+</body>
+
+</html>
