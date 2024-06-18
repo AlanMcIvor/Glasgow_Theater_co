3fd397266472205d415cb65c515546246f498988
Your Name <you@example.com>
2024-06-18 22:54:05 +0100

    carousel

--- pages/shared/index.php
+++ pages/shared/index.php
@@ -4,12 +4,63 @@ include "./includes/header.php"
 
 ?>
 
-<main class="hero">
-  <div class="hero_info">
-    <h3 class="hero_title">Grease: The Musical</h3>
-    <p class="hero_date">Tue 15 June - Sat 22 June</p>
-    <p class="hero_btns flex"><button class="btn"><span>Info</span></button><button class="btn main_btn">Tickets</button></p>
+<main>
+  <div class="hero">
+    <div class="hero_info">
+      <h3 class="hero_title">Grease: The Musical</h3>
+      <p class="hero_date">Tue 15 June - Sat 22 June</p>
+      <p class="hero_btns flex"><button class="btn"><span>Info</span></button><button class="btn main_btn">Tickets</button></p>
+    </div>
   </div>
+
+  <div class="carousel">
+    <div class="show_slide flex">
+      <div class="show_container">
+        <img class="show_img" src="../../assets/shows/r&j.png" alt="Rome and Julet show image">
+        <div class="show_txt_container">
+          <p class="show_title">Romeo & Juliet</p>
+          <p class="show_book">Book Now</p>
+        </div>
+      </div>
+      <div class="show_container">
+        <img class="show_img" src="../../assets/shows/r&j.png" alt="Rome and Julet show image">
+        <div class="show_txt_container">
+          <p class="show_title">Romeo & Juliet</p>
+          <p class="show_book">Book Now</p>
+        </div>
+      </div>
+      <div class="show_container">
+        <img class="show_img" src="../../assets/shows/r&j.png" alt="Rome and Julet show image">
+        <div class="show_txt_container">
+          <p class="show_title">Romeo & Juliet</p>
+          <p class="show_book">Book Now</p>
+        </div>
+      </div>
+      <div class="show_container">
+        <img class="show_img" src="../../assets/shows/r&j.png" alt="Rome and Julet show image">
+        <div class="show_txt_container">
+          <p class="show_title">Romeo & Juliet</p>
+          <p class="show_book">Book Now</p>
+        </div>
+      </div>
+      <div class="show_container">
+        <img class="show_img" src="../../assets/shows/r&j.png" alt="Rome and Julet show image">
+        <div class="show_txt_container">
+          <p class="show_title">Romeo & Juliet</p>
+          <p class="show_book">Book Now</p>
+        </div>
+      </div>
+      <div class="show_container">
+        <img class="show_img" src="../../assets/shows/r&j.png" alt="Rome and Julet show image">
+        <div class="show_txt_container">
+          <p class="show_title">Romeo & Juliet</p>
+          <p class="show_book">Book Now</p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
 </main>
 
 </body>
