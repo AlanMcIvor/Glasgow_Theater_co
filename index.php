a2fe39a57bca07166b3c8d78cfb070c63f02f22c
Your Name <you@example.com>
2024-07-25 22:21:40 +0100

    newsletter sign up

--- index.php
+++ index.php
@@ -18,64 +18,81 @@
       </div>
     </div>
   </section>
+
+
+  <section id="home_whats_on">
+    <div class="page_title">
+      <h3>What's On</h3>
+    </div>
+
+    <div class="card_container">
+      <div class="card">
+        <div class="card_img">
+          <img src="./assets/show_img/placeholder.png" alt="">
+        </div>
+        <div class="card_title">
+          <h4>Grease: The Musical</h4>
+        </div>
+        <div class="card_description">
+          <p>The world’s best-loved musical is back! Grittier and more glamorous than ever before.</p>
+        </div>
+        <div class="card_details">
+          <div class="card_date"><i class="fa-regular fa-calendar"></i><span>June 23rd, 8pm - 10pm</span></div>
+          <div class="card_genre"><i class="fa-solid fa-music"></i><span>Musical</span></div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card_img">
+          <img src="./assets/show_img/placeholder.png" alt="">
+        </div>
+        <div class="card_title">
+          <h4>Grease: The Musical</h4>
+        </div>
+        <div class="card_description">
+          <p>The world’s best-loved musical is back! Grittier and more glamorous than ever before.</p>
+        </div>
+        <div class="card_details">
+          <div class="card_date"><i class="fa-regular fa-calendar"></i><span>June 23rd, 8pm - 10pm</span></div>
+          <div class="card_genre"><i class="fa-solid fa-music"></i><span>Musical</span></div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card_img">
+          <img src="./assets/show_img/placeholder.png" alt="">
+        </div>
+        <div class="card_title">
+          <h4>Grease: The Musical</h4>
+        </div>
+        <div class="card_description">
+          <p>The world’s best-loved musical is back! Grittier and more glamorous than ever before.</p>
+        </div>
+        <div class="card_details">
+          <div class="card_date"><i class="fa-regular fa-calendar"></i><span>June 23rd, 8pm - 10pm</span></div>
+          <div class="card_genre"><i class="fa-solid fa-music"></i><span>Musical</span></div>
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+  <section class="newsletter" id="newsletter">
+    <div class="newsletter_title">
+      <h3>Keep up to date</h3>
+      <h4 class="subtitle">News and stories from backstage and special offers on a range of events and experiences</h4>
+
+      <form action="">
+        <div class="form_group">
+          <input type="email" id="newsletterEmail" name="newsletterEmail" placeholder="Enter your email...">
+          <button type="submit" class="btn primary_btn">Sign up</button>
+        </div>
+      </form>
+    </div>
+    </div>
+  </section>
+
 </main>
-
-<section id="home_whats_on">
-  <div class="page_title">
-    <h3>What's On</h3>
-  </div>
-
-  <div class="card_container">
-    <div class="card">
-      <div class="card_img">
-        <img src="./assets/show_img/placeholder.png" alt="">
-      </div>
-      <div class="card_title">
-        <h4>Grease: The Musical</h4>
-      </div>
-      <div class="card_description">
-        <p>The world’s best-loved musical is back! Grittier and more glamorous than ever before.</p>
-      </div>
-      <div class="card_details">
-        <div class="card_date"><i class="fa-regular fa-calendar"></i><span>June 23rd, 8pm - 10pm</span></div>
-        <div class="card_genre"><i class="fa-solid fa-music"></i><span>Musical</span></div>
-      </div>
-    </div>
-
-    <div class="card">
-      <div class="card_img">
-        <img src="./assets/show_img/placeholder.png" alt="">
-      </div>
-      <div class="card_title">
-        <h4>Grease: The Musical</h4>
-      </div>
-      <div class="card_description">
-        <p>The world’s best-loved musical is back! Grittier and more glamorous than ever before.</p>
-      </div>
-      <div class="card_details">
-        <div class="card_date"><i class="fa-regular fa-calendar"></i><span>June 23rd, 8pm - 10pm</span></div>
-        <div class="card_genre"><i class="fa-solid fa-music"></i><span>Musical</span></div>
-      </div>
-    </div>
-
-    <div class="card">
-      <div class="card_img">
-        <img src="./assets/show_img/placeholder.png" alt="">
-      </div>
-      <div class="card_title">
-        <h4>Grease: The Musical</h4>
-      </div>
-      <div class="card_description">
-        <p>The world’s best-loved musical is back! Grittier and more glamorous than ever before.</p>
-      </div>
-      <div class="card_details">
-        <div class="card_date"><i class="fa-regular fa-calendar"></i><span>June 23rd, 8pm - 10pm</span></div>
-        <div class="card_genre"><i class="fa-solid fa-music"></i><span>Musical</span></div>
-      </div>
-    </div>
-
-  </div>
-</section>
 
 <script src="./js/mobileMenu.js"></script>
 </body>
